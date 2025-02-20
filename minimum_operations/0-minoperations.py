bdc77a5931d7e1cb08a566b4db3600971a10c93a
clivetmushipe088 <c.mushipe@alustudent.com>
2025-02-20 19:18:16 +0300

    initial commit

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+def minOperations(n):
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    divisor = 2
+    
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n = n // divisor
+        divisor += 1
+    
+    return operations
+
+# Example usage:
+n = 9
+print(minOperations(n))  # Output: 6
