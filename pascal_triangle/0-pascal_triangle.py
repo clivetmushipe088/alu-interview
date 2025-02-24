18f5ede1a47318a808a4f0e7aa108d0416235ce7
clivetmushipe088 <c.mushipe@alustudent.com>
2025-02-24 18:44:55 +0300

    added code

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,11 @@
+def pascal_triangle(n):
+    """Generate Pascal's Triangle up to n rows."""
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+        triangle.append(row)
+    return triangle
